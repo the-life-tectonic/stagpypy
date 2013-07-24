d9488b04043ca1bf843ce203dc03e0ea652f5743
Robert Petersen <rpetersen@ucsd.edu>
2013-07-24 13:34:37 -0500

    Added gas constant

--- src/stagyy/constants.py
+++ src/stagyy/constants.py
@@ -1,4 +1,5 @@
 s_in_y=s_in_a=seconds_in_a_year=365.25*24*3600
 s_in_ma=seconds_in_million_years=1e6*365.25*24*3600
-g=9.807
+g=9.80665 # standard acceleration of gravity ( 
+R=8.3144621 # the gas constant, in J/K/mo  (http://physics.nist.gov/cgi-bin/cuu/Value?r)
 
