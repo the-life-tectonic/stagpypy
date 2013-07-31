640324df1efd0cb781d5dfabdaf69e941822e83e
Robert Petersen <rpetersen@ucsd.edu>
2013-07-31 07:01:50 -0700

    root finding function

--- src/stagyy/util.py
+++ src/stagyy/util.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.special import erf,erfinv
-from .constants import s_in_y
+from constants import s_in_y,R
 
 def cmy2ms(v):
 	return v/(100*s_in_y)
@@ -30,6 +30,45 @@ def age_hs(T0,T1,T,d,kappa):
 
 def Ra(rho,g,alpha,delta_T,length,eta,kappa):
 	return rho*g*alpha*delta_T*length**3/(eta*kappa)
+
+def boundary_layer(ra,ra_ref):
+    """Returns the relative boundary layer thickness for a system 
+    with Rayleigh Number Ra given a reference Ra number"""
+    return (ra_ref/ra)**(1./3.)
+
+def find_val(a,b,f,y=[0],err=0.001,maxits=100000):
+    """
+Finds x where f(x)=y between a and b.
+y may be a a scalar or list of values.
+err is the relative error.  A value is considered 
+equal if abs((f(x) - y)/y)<err.
+maxits is the maximum number of itterations.
+    """
+    # TODO
+    # This is a simple bisecting method (http://en.wikipedia.org/wiki/Bisection_method)
+    # Perhaps a secant method would work better (http://en.wikipedia.org/wiki/Secant_method)
+    p=[]
+    if not hasattr(y,'__iter__'):
+        y=[y]
+    for y0 in y:
+        a0=a
+        b0=b
+        it=0
+        x = (a0+b0)/2
+        v=f(x)
+        while it<maxits and abs(y0-v)>err*y0:
+            x = (a0+b0)/2
+            v=f(x)
+            if ((f(a0)-y0) * (v-y0)) > 0:
+                a0 = x
+            else:
+                b0 = x
+            it+=1
+        p.append(x) 
+    return p[0] if len(p)==1 else p
+
+def arrhenius(E,V,eta_ref,T_ref,p_ref):
+    return lambda T,p: eta_ref * np.exp( ((E+V*p)/(R*T)) - ((E+V*p_ref)/(R*T_ref)) ) 
 
 def combinations(*args):
 	c=[]
