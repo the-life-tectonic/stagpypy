e648c2a0a1236e384f52e2a2237ee712740245bd
robert <rpetersen@ucsd.edu>
2015-02-11 09:00:15 -0800

    Added interpoloation function which interpolates a refined grid into a regular grix for the XZ plane

--- src/stagyy/geometry.py
+++ src/stagyy/geometry.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import newton
+from scipy import interpolate
 
 def expint(x):
 #real function expint (x)
@@ -17,8 +18,8 @@ def g_of_z_exp(z,layers=[],dresl_topbot=1.0):
     """
     Each layer is a tuple:
     (normalized depth, dresl, wresl)
-    Normalized depth is layer depth/domain deption [0:1]
-    dresl is the approzimate refinement
+    Normalized depth is layer depth/domain depth [0:1]
+    dresl is the approximate refinement
     wresl is the width
     """
     a=2.0/(1.0+dresl_topbot)
@@ -85,6 +86,44 @@ For a function, y=f(x) and values y, find x such that  f(x)-y<errval
         x=0.5*(xlo+xhi)
         fx=f(x)
     return x
+
+def interpolate_h5_xz(h5,Lx,Lz):
+    frames,nx,ny,nz=h5['data'].shape
+    x=h5['x']
+    z=h5['z']
+    px=nx
+    pz=int(nx*Lz/Lx)
+    dx=Lx/px
+    dz=Lz/pz
+    x_new=(np.arange(px)+.5)*dx
+    z_new=(np.arange(pz)+.5)*dz
+
+    #  Create the image group if it doesn't exist
+    if not 'image' in h5:
+        img=h5.create_group('image')
+        for dset_name in ['data','p','v','vx','vy','vz']:
+            if dset_name in h5: img.create_dataset(dset_name, (0,px,pz),compression='gzip', compression_opts=4,maxshape=(None,px,pz))
+        # Create the x and z datasets
+        img.create_dataset('x', data=x_new,compression='gzip', compression_opts=4)
+        img.create_dataset('z', data=z_new,compression='gzip', compression_opts=4)
+        # link the frames, and y points
+        img['y']=h5['y']
+        img['frame']=h5['frame']
+    else:
+        img=h5['image']
+    
+    for dset_name in ['data','p','v','vx','vy','vz']:
+        if dset_name in h5:
+            data_set=h5[dset_name]
+            img_set=img[dset_name]
+            data_frames=data_set.shape[0]
+            img_frames=img_set.shape[0]
+            img_set.attrs['min']=data_set.attrs['min']
+            img_set.attrs['max']=data_set.attrs['max']
+            img_set.resize((data_frames-img_frames,px,pz))
+            for n in xrange(img_frames,data_frames):
+                f=interpolate.interp2d(z,x,np.squeeze(data_set[n]))
+                img_set[n]=f(z_new,x_new)
 
 class Grid(object):
     def __init__(self,N,L):
